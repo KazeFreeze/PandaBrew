08a0ef75d30b19ecb671be3f90fbda5c8fec6f46
Bernard G. Tapiru, Jr. <tapirua@gmail.com>
2025-07-21 23:09:07 +0800

    feat: add basic linux support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ttkbootstrap as ttkb
 from app.app import ModernCodeExtractorGUI
-import pywinstyles
+import sys
 
 if __name__ == "__main__":
     """
@@ -9,10 +9,18 @@ if __name__ == "__main__":
     # Use ttkbootstrap Window with the 'darkly' theme
     root = ttkb.Window(themename="darkly")
 
-    # Apply a dark, mica style to the window for better performance
-    pywinstyles.apply_style(root, "mica")
-    # Set a lighter, less saturated blue tint color
-    pywinstyles.set_opacity(root, color="#2c3e50")
+    # Conditionally apply Windows-specific styling for a native look and feel
+    if sys.platform == "win32":
+        try:
+            import pywinstyles
+            # Apply a dark, mica style to the window for better performance
+            pywinstyles.apply_style(root, "mica")
+            # Set a lighter, less saturated blue tint color
+            pywinstyles.set_opacity(root, color="#2c3e50")
+            print("Applied Windows-specific styling.")
+        except ImportError:
+            print("pywinstyles not found, skipping Windows-specific styling.")
+
 
     app = ModernCodeExtractorGUI(root)
     root.protocol("WM_DELETE_WINDOW", app.on_closing)
